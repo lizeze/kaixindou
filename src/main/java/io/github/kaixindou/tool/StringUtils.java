3fc0534ff7f0b9162d65d133139eddbd116495ec
zeze.li <lizeze.boy@gmail.com>
2021-08-05 10:18:29 +0800

    first commit

---
+++ src/main/java/io/github/kaixindou/tool/StringUtils.java
@@ -0,0 +1,18 @@
+package io.github.kaixindou.tool;
+
+
+public class StringUtils {
+
+
+    public static boolean isNull() {
+
+
+        return "".equals("2");
+    }
+
+    public static boolean isNotNull() {
+
+
+        return isNull() == false;
+    }
+}
